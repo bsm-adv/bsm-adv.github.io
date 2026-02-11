738bb5e1bdb6e3c2c1eca0f257a84dd2fb4c2b05
zmni <zmni@users.noreply.github.com>
2026-02-12 01:03:13 +0700

    bsm

---
+++ _astro/dom.BjsoGNXY.js
@@ -0,0 +1,1 @@
+const t=(e,n=document)=>{const o=n.querySelector(e);return!o&&typeof window<"u"&&console.warn(`Element not found: ${e}`),o},r=(e,n=document)=>Array.from(n.querySelectorAll(e));export{t as $,r as a};
